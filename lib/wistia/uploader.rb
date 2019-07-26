8532731f47cbea7390a267a5461338889db32ed0
Sharon Evans-Durkin <sharon@ameritest.com>
2019-07-26 11:57:49 -0600

    Uploader
    * Added an upload method
    * Trying to bump ruby version to get http/post/multipart to work (but not currently working)

--- lib/wistia/uploader.rb
+++ lib/wistia/uploader.rb
@@ -1,8 +1,21 @@
 module Wistia
   class Uploader < Wistia::Base
 
+    # require 'net/https'
+    # require 'net/http/post/mutipart'
+
     self.site = "https://upload.wistia.com/"
 
+    
+    def self.upload(attributes={})
+      attributes.merge!('api_password' => Wistia.password)
+      http = Net::HTTP.new(URI(self.site).host, URI(self.site).port)
+      http.use_ssl = true
+
+      request = Net::HTTP::Post::Multipart.new uri.request_uri, attributes
+      response = http.request(request)
+
+    end # upload
 
     # def self.new(attributes={}, persisted = false)
     #   if !attributes.key?(:file)
